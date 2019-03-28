40e54a903f71132bb0b684def7112408acf7f937
sagarb96 <sbsagariscool3@gmail.com>
2019-03-28 22:15:07 +0530

    The categorical data has been encoded and divided into dummy columns wherever needed.

--- Data-Prepocessing/data-preprocessing-template.py
+++ Data-Prepocessing/data-preprocessing-template.py
@@ -22,3 +22,15 @@ imputer = imputer.fit(X[:, 1:3]) #fitting the imputer object on
                                 #column 1 & 2 of X.
 X[:, 1:3] = imputer.transform(X[:, 1:3]) #replaces missing data 
                                         #with mean
+                                        
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0]) #column 0 is categorical
+onehotencoder = OneHotEncoder(categorical_features = [0]) 
+X = onehotencoder.fit_transform(X).toarray() #to include dummy columns
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y) #dependent variable array is categorical
+#Note: Not required to apply OneHotEncoder on dependent variable
+#       as model already knows it's categorical and has no order.
+
