ff5b8c6e3664d49a793d9b346d6e00e5c26ab120
sagarb96 <sbsagariscool3@gmail.com>
2019-03-29 13:13:12 +0530

    Data split into Training and Test set. Feature scaling is applied.

--- Data-Prepocessing/data-preprocessing-template.py
+++ Data-Prepocessing/data-preprocessing-template.py
@@ -5,12 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 #Importing the data from Data.csv
 dataset = pd.read_csv('Data.csv')
-#independent variables
-X = dataset.iloc[:, :-1].values
-#dependent variable
-y = dataset.iloc[:, 3].values
+X = dataset.iloc[:, :-1].values #independent variables
+y = dataset.iloc[:, 3].values #dependent variable
+
 
 #Taking care of missing data
 #Importing "preprocessing" library from Scikit LearnImputer
@@ -22,6 +22,7 @@ imputer = imputer.fit(X[:, 1:3]) #fitting the imputer object on
                                 #column 1 & 2 of X.
 X[:, 1:3] = imputer.transform(X[:, 1:3]) #replaces missing data 
                                         #with mean
+
                                         
 #Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -34,3 +35,14 @@ y = labelencoder_y.fit_transform(y) #dependent variable array is categorical
 #Note: Not required to apply OneHotEncoder on dependent variable
 #       as model already knows it's categorical and has no order.
 
+
+#Splitting the dataset into the Training set and Testing set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2,
+                                                    random_state = 0)
+
+#Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
