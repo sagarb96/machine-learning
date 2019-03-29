043c7dbc080efe576dac5f8755df4c8ce3cc5474
sagarb96 <sbsagariscool3@gmail.com>
2019-03-29 18:16:21 +0530

    General purpose template added for going ahead with all data preprocessing jobs in the upcoming sections.

---
+++ data-preprocessing-template.py
@@ -0,0 +1,24 @@
+#Template for date preprocessing
+
+#Importing the libraries - numpy, matplotlib.pyplot & pandas
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+#Importing the data from Data.csv
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values 
+y = dataset.iloc[:, 3].values
+
+
+#Splitting the dataset into the Training set and Testing set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2,
+                                                    random_state = 0)
+
+#Feature scaling
+'''from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)'''
