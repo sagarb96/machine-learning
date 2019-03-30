6030ac4e928408aa95764415a076bf862bc82a2c
sagarb96 <sbsagariscool3@gmail.com>
2019-03-30 21:01:01 +0530

    Predicted the test set and comapred the outcomes using plots

--- Simple-Linear-Regression/simple-linear-regression.py
+++ Simple-Linear-Regression/simple-linear-regression.py
@@ -15,13 +15,6 @@ y = dataset.iloc[:, 1].values   #vector
 #Splitting the dataset into the Training set and Testing set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3,
-                                                    random_state = 0)
-
-#Feature scaling
-'''from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)'''
 
 
 #Fitting Simple Linear Regression to the Training set
@@ -30,3 +23,23 @@ regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 
+#Predicting the Test set results
+y_predicted = regressor.predict(X_test)
+
+
+#Visualising the Training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience(Training set)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
+ 
+
+#Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience(Test set)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
