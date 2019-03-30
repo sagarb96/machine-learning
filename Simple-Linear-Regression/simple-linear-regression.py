950cc5324b1e00184a91857285fe50b65e9144ab
sagarb96 <sbsagariscool3@gmail.com>
2019-03-30 20:27:22 +0530

    Data has been preprocessed

--- Simple-Linear-Regression/simple-linear-regression.py
+++ Simple-Linear-Regression/simple-linear-regression.py
@@ -1,4 +1,4 @@
-#Template for date preprocessing
+#Simple Linear Regression
 
 #Importing the libraries - numpy, matplotlib.pyplot & pandas
 import numpy as np
@@ -7,14 +7,14 @@ import pandas as pd
 
 
 #Importing the data from Data.csv
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values 
-y = dataset.iloc[:, 3].values
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values #matrix
+y = dataset.iloc[:, 1].values   #vector
 
 
 #Splitting the dataset into the Training set and Testing set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2,
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3,
                                                     random_state = 0)
 
 #Feature scaling
@@ -22,3 +22,4 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2,
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)'''
+
